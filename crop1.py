ff0de696a444b1de1eb261269ea6123ff118d388
Zhou Mengyuan <zhou_me@worksap.co.jp>
2019-07-09 18:30:01 +0800

    2019/7/9 revise code

--- crop1.py
+++ crop1.py
@@ -1,6 +1,7 @@
 # import the necessary packages
 import numpy as np
 import imutils
+from imutils import contours
 import cv2
 
 def is_contour_bad(c):
@@ -12,26 +13,56 @@ def is_contour_bad(c):
     return len(approx) > 3
 
 # load the shapes image, convert it to grayscale, and edge edges in the image
-image = cv2.imread('test.png')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-edged = cv2.Canny(gray, 50, 100)
+image = cv2.imread('test_new.tif')
+b, g, r = cv2.split(image)
+cv2.imwrite('new_r.png', r)
+tmp_image = r
+# gray = cv2.cvtColor(tmp_image, cv2.COLOR_BGR2GRAY)
+# edged = cv2.Canny(gray, 50, 100)
 # cv2.imshow('Original', image)
 # cv2.imwrite('temp.png', image)
 
 # find contours in the image and initialize the mask that will be used to remove the bad contours
-cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cv2.findContours(tmp_image.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+# cnts = imutils.grab_contours(cnts)
 cnts = imutils.grab_contours(cnts)
 mask = np.ones(image.shape[:2], dtype='uint8') * 255
-
+# mask = np.ones(image)
+# sort_contours(cnts, method="left-to-right"):
 # loop over the contours
+num = 0
 for c in cnts:
     # if the contour is bad, draw it on the mask
-    if is_contour_bad(c):
-        cv2.drawContours(mask, [c], -1, 0, -1)
+    num += 1
+    cv2.drawContours(mask, [c], -1, 0, -1)
+    #if is_contour_bad(c):
+        #cv2.drawContours(mask, [c], -1, 0, -1)
+        # mask = np.zeros(image.shape).astype(image.dtype)
+        # color = [255, 255, 255]
+        # cv2.fillPoly(mask, [c], color)
+        # result = cv2.bitwise_and(image, mask)
+        #
+        # cv2.imwrite('temp' + str(num) + '.png', result)
+    cv2.imwrite('temp'+str(num) + '.png', mask)
+
+# loop over the sorting methods
+# for method in ("left-to-right", "right-to-left", "top-to-bottom", "bottom-to-top"):
+#     num += 1
+#     # sort the contours
+#     (cnts, boundingBoxes) = contours.sort_contours(cnts, method=method)
+#     clone = image.copy()
+#
+#     # loop over the sorted contours and label them
+#     for (i, c) in enumerate(cnts):
+#         sortedImage = contours.label_contour(clone, c, i)
+#
+#     # show the sorted contour image
+#     cv2.imwrite('temp' + str(num) + '.png', sortedImage)
+#     #cv2.imshow(method, sortedImage)
 
 # remove the contours from the image and show the resulting images
 # image = cv2.bitwise_and(image, image, mask=mask)
 # cv2.imshow('Mask', mask)
-cv2.imwrite('re1.png', mask)
+cv2.imwrite('new1.png', mask)
 # cv2.imshow('After', image)
 cv2.waitKey(0)
--- main.py
+++ main.py
@@ -9,33 +9,115 @@
 @software: PyCharm
 """
 import numpy as np
+import imutils
+from imutils import contours
 import cv2
+import os
+import sys
 
-# 设定文件路径
-img_path = './exam.png'
-# Read image
-img = cv2.imread(img_path)
+# For every original image, generate a folder named by it's filename to store it's split images
+# Do not leave any blank area about the lines, they have to be crossed. and pay attention to
+# small area that might have circle.
 
-# 创建一个窗口
-cv2.namedWindow("image", flags=cv2.WINDOW_NORMAL | cv2.WINDOW_FREERATIO)
-cv2.namedWindow("image_roi", flags=cv2.WINDOW_NORMAL | cv2.WINDOW_FREERATIO)
+input_images_dir = 'C:\\xyq\\splitGraph\\example'  # file format should be '*.tif'
+output_images_names_prefix = ['VZ', 'ISVZ', 'OSVZ', 'IZ+CP']  # output filename rules, from the inside out.
 
-cv2.imshow("image", img)
-# # 是否显示网格
-# showCrosshair=True
-# # 如果为Ture的话 , 则鼠标的其实位置就作为了roi的中心
-# # False: 从左上角到右下角选中区域
-# fromCenter = False
-# # Select ROI
-# rect = cv2.selectROI("image", img, showCrosshair, fromCenter)
-#
-# print("选中矩形区域")
-# (x, y, w, h) = rect
-#
-# # Crop image
-# imCrop = img[y:y+h, x:x+w]
-#
-# # Display cropped image
-# cv2.imshow("image_roi", imCrop)
-# cv2.imwrite("image_roi.png", imCrop)
-cv2.waitKey(0)
+
+def init():
+    """"
+    load the input images, return the list of file name
+    """
+    result_list_files = []
+    files = os.listdir(input_images_dir)
+    for i in files:
+        if os.path.splitext(i)[1] == '.tif':
+            result_list_files.append(i)
+
+    return result_list_files
+
+
+def split_graph(file_name, output_dir):
+    image = cv2.imread(file_name)
+    b, g, r = cv2.split(image)
+    tmp_image = r
+
+    # find contours in the image and initialize the mask that will be used to remove the bad contours
+    tmp_cnts = cv2.findContours(tmp_image.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(tmp_cnts)
+
+    # 0 means clockwise, 1 means counterclockwise
+    save_splits(image, cnts, output_dir, 0)
+
+    num = 0
+    color = [255, 255, 255]
+    for c in cnts:
+        cv2.drawContours(image, [c], -1, (0, 255, 0), 1)
+        mask = np.zeros(image.shape).astype(image.dtype)
+        cv2.fillPoly(mask, [c], color)
+        tmp_result = cv2.bitwise_and(image, mask)
+        try:
+            result = rect_splits(tmp_result, c)
+            if not(type(result) is str) and not(result == ''):
+                num += 1
+                cv2.imwrite(output_dir + '/temp' + str(num) + '.png', result)
+        except BaseException:
+            print(BaseException)
+            sys.exit(0)
+
+
+def rect_splits(image, contours):
+    """
+    return minimize rectangle area for the contours
+    """
+    (x, y, w, h) = cv2.boundingRect(contours)
+    rect = ''
+    if h > 5 and w > 5:
+        rect = image[y:y + h, x:x + w]
+    return rect
+
+
+def save_splits(image, cnts, output_dir, seq):
+    """
+    Define how to name these splits graph, clockwise or unclockwise
+    clockwise 0
+    counterclockwise 1
+    """
+    if seq == 0:
+        (cnts, boundingBoxes) = contours.sort_contours(cnts, method='left-to-right')
+    elif seq == 1:
+        (cnts, boundingBoxes) = contours.sort_contours(cnts, method='right-to-left')
+
+    # sort the contours
+    clone = image.copy()
+
+    # loop over the sorted contours and label them
+    for (i, c) in enumerate(cnts):
+        sortedImage = contours.label_contour(clone, c, i)
+
+    # show the sorted contour image
+    cv2.imwrite(output_dir + '/res' + str(seq) + '.tif', sortedImage)
+
+
+def process():
+    """"
+    1. load files
+    2. find Contours and draw contours, crop image
+    3. minimize all images, and save to local
+    """
+    files = init()
+    for file in files:
+        file_name = os.path.splitext(file)[0]    # filename without suffix
+        output_images_dir = input_images_dir + '\\' + file_name
+        is_exists = os.path.exists(output_images_dir)
+        if not is_exists:
+            try:
+                os.makedirs(output_images_dir)
+            except OSError as exception:
+                print(exception)
+                sys.exit(0)
+        split_graph(file, output_images_dir)
+
+
+if __name__ == '__main__':
+    process()
+    cv2.waitKey(0)
