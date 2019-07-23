08f374553d98208fd213286d0e0ac9b6401a028b
zmy <zhouyongpu@126.com>
2019-07-23 09:27:05 +0800

    revise calulateArea

--- calculateArea.py
+++ calculateArea.py
@@ -5,32 +5,39 @@
 # @Site :  
 # @File : calculateArea.py 
 # @Software: PyCharm
-import numpy as np
 import imutils
 from imutils import contours
 import cv2
 import os
 import sys
 
-input_images_dir = 'E:\\zn test'  # file format should be '*.tif'
+# input_images_dir = ''  # 'E:\\zn\\WT-1&2 brain'  # file format should be '*.tif'
 # unit area size
-unit_area_size = 0.207 * 0.207
+# u_para = ''
+# unit_area_size = 0.0  # 0.207 * 0.207
 cnts_list = []
 
-def init():
+
+def init(input_images_dir,u_para):
     """"
     load the input images, return the list of file name
     """
+    try:
+        unit_area_size = float(u_para) * float(u_para)
+    except Exception as exp:
+        print('Please input right number of pixel size. ' + exp)
+        sys.exit(0)
+
     result_list_files = []
     files = os.listdir(input_images_dir)
     for i in files:
         if os.path.splitext(i)[1] == '.tif':
             result_list_files.append(i)
 
-    return result_list_files
+    return result_list_files,unit_area_size
 
 
-def calculate_area(file, output_images_dir):
+def calculate_area(file, input_images_dir, output_images_dir,unit_area_size):
     file_name = input_images_dir + '\\' + file
     print(file_name)
     image = cv2.imread(file_name, 0)
@@ -53,20 +60,21 @@ def calculate_area(file, output_images_dir):
         try:
             result = rect_splits(image, c)
             if not (result == ''):
-                (cx, cy), radius = cv2.minEnclosingCircle(c)
-                center = (int(cx), int(cy))
+                # (cx, cy), radius = cv2.minEnclosingCircle(c)
+                # center = (int(cx),int(cy))
                 area_num = cv2.contourArea(c, False) * unit_area_size
                 # print(area_num)
                 #
-                # M = cv2.moments(c)
-                # (cx, cy) = M['m10'] / M['m00'], M['m01'] / M['m00']
-                # center = (int(cx), int(cy))
+                M = cv2.moments(c)
+                (cx, cy) = (M['m10'] / M['m00'], M['m01'] / M['m00'])
+                center = (int(cx), int(cy))
                 # radius = int(radius)
                 # cv2.circle(image, center, radius, (255, 255, 255), 3)
                 font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(image, str(round(area_num, 2)) + 'um^2', center, font, 1.2, (255, 255, 255), 2)
+                cv2.putText(image, str(round(area_num, 2)), center, font, 1.2, (255, 255, 255), 2)
         except Exception as exp:
             print(exp)
+            sys.exit(0)
 
     cv2.imwrite(output_images_dir + '//' + file[:-3] + '.tif', image)
 
@@ -80,7 +88,6 @@ def rect_splits(image, contours):
     if h > 20 and w > 20:
         rect = image.copy()[y:y + h, x:x + w]
     return rect
-
 
 
 def sort_splits(cnts):
@@ -108,14 +115,13 @@ def sort_splits(cnts):
     return ll
 
 
-
-def process():
+def process(input_images_dir, u_para):
     """"
     1. load files
     2. find Contours and draw contours, crop image
     3. minimize all images, and save to local
     """
-    files = init()
+    (files, unit_area_size) = init(input_images_dir, u_para)
     for file in files:
         file_name = os.path.splitext(file)[0]    # filename without suffix
         output_images_dir = input_images_dir + '\\' + file_name
@@ -127,9 +133,22 @@ def process():
                 print(exception)
                 sys.exit(0)
 
-        calculate_area(file, output_images_dir)
+        calculate_area(file, input_images_dir, output_images_dir,unit_area_size)
+
+
+def main(argv):
+    if len(argv) < 2:
+        print('Please input at least two parameters, the first one is the input directory, the second one is pixel size.')
+        sys.exit(0)
+
+    input_images_dir = argv[1]
+    u_para = argv[2]
+    process(input_images_dir, u_para)
+    cv2.waitKey(0)
 
 
 if __name__ == '__main__':
-    process()
-    cv2.waitKey(0)
+    print('Please input two parameters:')
+    print('1. the input directory. For example, E:\\zh\\test ')
+    print('2. pixel size. For example, 0.207')
+    main(sys.argv)
